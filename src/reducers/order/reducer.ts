66387b3cfdc296ec15497e984250befc5134e57f
ezequiel <ezequiel.alves@souunit.com.br>
2022-07-24 21:47:47 -0300

    reducers adjusts

--- src/reducers/order/reducer.ts
+++ src/reducers/order/reducer.ts
@@ -40,7 +40,7 @@ export function orderReducer(state: OrderState, action: actionType) {
 
             return {
               ...product,
-              quantity: oldQuantity + productAlreadyChoosed.quantity
+              quantity: oldQuantity + action.payload.product.quantity
             }
           }
 
